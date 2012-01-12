c138d836a57bc9816367631fdd4bcd811ed43f86
Vugar Afandiyev <evugar@gmail.com>
2012-01-12 22:47:15 +0100

    Added back

---
+++ main.asm
@@ -0,0 +1,122 @@
+	list p=pic12f629
+	#include p12f629.inc
+
+	__config _WDT_OFF & _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & _CP_OFF & _CPD_OFF
+	radix dec
+
+	#include macros.inc
+
+	extern add_task, process_queue, do_task
+
+	extern timer_delay
+	extern add_timer_task, timer_process, init_timer_queue
+	
+
+	
+pin_SER			equ 0
+pin_SERCLK		equ 1
+pin_RCLK		equ 2
+
+
+reset_vector	code	0x0
+		goto start
+
+int_save_reg		udata
+_save_w res 1
+_save_FSR res 1
+_save_STATUS res 1
+
+		
+int_vector		code	0x4
+		
+		movwf _save_w ;copy W to temp register, could be in either bank
+		swapf STATUS, w ;swap status to be saved into W
+		movwf _save_STATUS ;save status to bank 0 register
+		movfw FSR
+		movwf _save_FSR
+
+		call timer_process
+
+		movfw _save_FSR
+		movwf FSR
+		swapf _save_STATUS, w ;swap STATUS_TEMP register into W, sets bank to original state
+		movwf STATUS ;move W into STATUS register
+		swapf _save_w,F ;swap W_TEMP
+		swapf _save_w,W ;swap W_TEMP into W		
+
+		bcf INTCON, T0IF
+		
+		retfie
+
+main_prog	code
+
+;--------------------------------
+idle
+		retlw 0
+
+;-----------------------------
+pin_SER_on
+		bsf		GPIO, pin_SER
+		bsf		GPIO, pin_SERCLK
+		bcf		GPIO, pin_SERCLK
+		incf 	led_delay, f
+		movfw	led_delay
+		movwf	timer_delay
+		movlw TS_pin_debug_off
+		call	add_timer_task
+		retlw 0
+		
+;-------------------------
+byte_out
+		retlw 0
+
+;--------------------------------------
+TaskProcs
+
+TS_idle equ 0
+		goto	idle
+
+TS_pin_debug_on equ 1
+		goto	pin_debug_on
+		
+
+	global TaskProcs
+
+
+		
+				
+;----------------------------------
+init_hw
+	bsf		STATUS, RP0
+
+	movlw b'00000000'		; prescaler 
+	banksel OPTION_REG
+	movwf	OPTION_REG
+
+	clrf TRISIO				;GP0 - strobe
+	movlw b'11001011'		;GP1 - data
+	movwf TRISIO				;GP2 - clock
+
+	bcf		STATUS, RP0
+	
+	bsf INTCON, T0IE
+	bsf INTCON, GIE
+	retlw 0
+	
+
+;---------------------------------
+start
+		call init_timer_queue
+		
+;		movlw TS_pin_debug_on
+;		call add_task
+		movlw b'01010101'
+		call byte_out
+			
+			
+		call init_hw
+			
+loop_forever
+		call process_queue
+		goto loop_forever
+		end
---
+++ rtos.asm
@@ -0,0 +1,184 @@
+	list p=pic12f629
+	#include p12f629.inc
+
+	radix dec
+	
+	#include macros.inc
+
+	extern TaskProcs
+
+	udata_ovr
+temp1 res 1
+
+queue_size	equ	10
+
+tskq_data		udata
+offset_low	res	1
+
+addtsk_temp1 res 1
+pq_temp res 1
+queue_head	res 1
+queue_tail	res 1
+task_queue	res	queue_size
+
+tskq_proc	code
+
+;------------------------------------
+do_task
+		movwf	offset_low
+		movlw 	LOW TaskProcs
+		addwf	offset_low, f
+		movlw	HIGH TaskProcs
+		btfsc	STATUS, C
+		addlw	1
+		movwf	PCLATH
+		movfw	offset_low
+		movwf	PCL
+
+
+
+;-------------------------
+add_task
+		movwf	addtsk_temp1
+		movlw	task_queue
+		addwf	queue_tail, w
+		movwf	FSR
+		movfw	addtsk_temp1
+		movwf	INDF
+
+		movfw	queue_tail
+		xorlw	queue_size-1
+		je		tail_at_end
+		incf	queue_tail,f
+		goto at_proceed
+tail_at_end
+		clrf	queue_tail
+at_proceed
+
+		retlw 0		
+;--------------------------------------
+
+;------------------------------
+process_queue
+		movfw	queue_head
+		subwf	queue_tail, w
+		jne	pq_not_empty
+		call do_task  ; W already = 0 so run idle process (TS_idle)
+		retlw 0
+
+pq_not_empty
+		movlw	task_queue
+		addwf queue_head, w
+		movwf FSR
+		movfw INDF
+		call do_task
+	
+		incf queue_head, f
+		movfw queue_head
+		xorlw queue_size
+		skne
+		clrf queue_head
+		
+		retlw 0
+;-------------------------------------
+
+
+	global add_task, process_queue, do_task
+
+
+
+timer_queue_size	equ	5
+
+timq_data	udata
+att_temp1 res 1
+timer_delay res 1
+timer_task res 1
+tp_cur_entry res 1
+tp_save_task_id res 1
+timer_queue res timer_queue_size * 2
+
+	global timer_delay
+
+;	global att_temp1, timer_delay, timer_task, tp_cur_entry 
+;	global tp_save_task_id, timer_queue 
+;
+
+timq_proc		code
+;------------------------
+add_timer_task
+		movwf	timer_task
+		movlw timer_queue
+		movwf att_temp1
+att_loop1
+		movfw	att_temp1
+		movwf	FSR
+		movfw	INDF
+		xorlw 0xff
+		jne	att_next_cell
+		movfw	att_temp1
+;		bcf INTCON, GIE
+		movwf FSR
+		movfw timer_task
+		movwf INDF
+		incf FSR,f
+		movfw timer_delay
+		movwf INDF
+;		bsf INTCON, GIE
+		retlw 0
+att_next_cell
+		incf att_temp1, f
+		incf att_temp1, f
+		movfw att_temp1
+		xorlw timer_queue+timer_queue_size*2
+		jne att_loop1
+		retlw 0
+		
+;---------------------------------
+timer_process
+		movlw timer_queue
+		movwf tp_cur_entry
+		
+tp_loop1
+		movfw tp_cur_entry
+		movwf FSR
+		movfw INDF
+		xorlw 0xff
+		je tp_wait_more
+		incf FSR,f
+		decfsz INDF,f
+		goto tp_wait_more
+		decf FSR,f
+		movfw INDF
+		movwf tp_save_task_id
+		movlw 0xff
+		movwf INDF
+		movfw tp_save_task_id
+		call add_task
+tp_wait_more
+		incf tp_cur_entry, f
+		incf tp_cur_entry, f
+		movfw tp_cur_entry
+		xorlw timer_queue+timer_queue_size*2
+		jne tp_loop1
+		retlw 0
+
+;------------------
+init_timer_queue
+		movlw timer_queue
+		movwf temp1
+itq_loop1		
+		movfw temp1
+		movwf FSR
+		movlw 0xff
+		movwf INDF
+		incf temp1, f
+		movfw temp1
+		xorlw timer_queue+timer_queue_size*2
+		jne itq_loop1
+		retlw 0
+	
+
+	global add_timer_task, timer_process, init_timer_queue
+
+
+	end
